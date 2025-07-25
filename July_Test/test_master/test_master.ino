839ab774c15866dc78b37b18fe57b44b0b3c984e
Becky Button <EinsteinUnicorn@users.noreply.github.com>
2025-07-25 15:02:30 -0400

    brain

---
+++ July_Test/test_master/test_master.ino
@@ -0,0 +1,70 @@
+#include <SBWire.h>
+
+
+volatile uint8_t TARGET_BLOCK =  0x7C; // I2C address of the program block
+int addresses [127];
+int i = 0;
+
+void setup() {
+  Wire.begin(); // Initialize as master
+  Wire.setTimeout(1000); //timeout value in uSec - SBWire uses 100 uSec, so 1000 should be OK
+  Serial.begin(9600); // For debugging
+  delay(1000); // Allow time for initialization
+  Serial.println("Brain block initialized.");
+  pinMode(LED_BUILTIN, OUTPUT);
+  memset(addresses,0,sizeof(addresses));
+  addresses[0] = (int)TARGET_BLOCK;
+}
+
+int missedI2Crequest = 0;
+void loop() {
+  Wire.requestFrom((int)TARGET_BLOCK, 1, true);
+  delay(150); 
+  
+  if (Wire.available() >= 1) { 
+    i++;
+    Serial.println("target address:");
+    Serial.println(TARGET_BLOCK);
+
+    uint8_t nextAddress = Wire.read();
+
+    TARGET_BLOCK = nextAddress;
+    Serial.println("Next address:");
+    Serial.println(nextAddress);
+
+    addresses[i] = (int)nextAddress;
+    missedI2Crequest = 0;
+  } else {
+    missedI2Crequest++;
+    Serial.println(missedI2Crequest);
+    //Serial.println("Nothing else connected");
+  }
+
+  if(missedI2Crequest == 10){
+    String compiledProgram = createProgram();
+    Serial.print(compiledProgram);
+  }
+
+  delay(500);
+}
+
+String createProgram() {
+  //Serial.println("In create program");
+  String program = "";
+  for(int j = 0; j < i; j++){
+    switch((int)addresses[j]){
+      case 86:
+         program += "start \n";
+         break;
+      case 124:
+         program += "middle \n";
+         break;
+      case 105:
+        program += "end \n";
+        break;
+      default:
+        break;
+    }
+  }
+  return program;
+}
