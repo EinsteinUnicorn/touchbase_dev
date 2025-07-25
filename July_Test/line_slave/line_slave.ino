fff2da5752ec7ddb7ab162014f798e68987b3370
Becky Button <EinsteinUnicorn@users.noreply.github.com>
2025-07-25 15:27:13 -0400

    blocks have been changes

--- July_Test/line_slave/line_slave.ino
+++ July_Test/line_slave/line_slave.ino
@@ -2,6 +2,7 @@
 
 #define ADC_PIN_CURR 3          // Pin used for the current block's ADC
 #define ADC_PIN_NEXT 2         // Pin for the next block's ADC
+#define ADC_PIN_NEXT_LINE 5
 
 int currAdcValue = 0;
 
@@ -10,6 +11,7 @@ int LED_PIN = 1;
 void setup() {
   pinMode(ADC_PIN_CURR, INPUT);           // Configure ADC pin as input
   pinMode(ADC_PIN_NEXT, INPUT);           // Configure ADC pin as input
+  pinMode(ADC_PIN_NEXT_LINE, INPUT);
 
   currAdcValue = analogRead(ADC_PIN_CURR); // Read ADC value
 
@@ -17,6 +19,7 @@ void setup() {
 
   TinyWireS.begin(currAddress); // Join the I2C bus as a slave
   TinyWireS.onRequest(sendADCValue);      // Register the request handler
+  TinyWireS.onRequest(sendNextLine);
 }
 
 void loop() {
@@ -27,5 +30,10 @@ void sendADCValue() {
   int nextAdcValue = analogRead(ADC_PIN_NEXT);   // Read ADC value
   uint8_t nextAddress = map(nextAdcValue, 0, 1023, 0, 127);
   TinyWireS.write(nextAddress);
+}
 
+void sendNextLine(){
+  int nextLineValue = analogRead(ADC_PIN_NEXT_LINE);
+  uint8_t nextLineAddress = map(nextLineValue, 0, 1023, 0, 127);
+  TinyWireS.write(nextLineAddress);
 }
--- July_Test/variable_slave/variable_slave.ino
+++ July_Test/variable_slave/variable_slave.ino
@@ -2,14 +2,17 @@
 
 #define ADC_PIN_CURR 3          // Pin used for the current block's ADC
 #define ADC_PIN_NEXT 2         // Pin for the next block's ADC
+#define ADC_PIN_VAR 5
 
 int currAdcValue = 0;
+int varAdcValue = 0; 
 
 int LED_PIN = 1;
 
 void setup() {
   pinMode(ADC_PIN_CURR, INPUT);           // Configure ADC pin as input
   pinMode(ADC_PIN_NEXT, INPUT);           // Configure ADC pin as input
+  pinMode(ADC_PIN_VAR, INPUT);
 
   currAdcValue = analogRead(ADC_PIN_CURR); // Read ADC value
 
@@ -17,6 +20,8 @@ void setup() {
 
   TinyWireS.begin(currAddress); // Join the I2C bus as a slave
   TinyWireS.onRequest(sendADCValue);      // Register the request handler
+  TinyWireS.onRequest(sendVarValue);
+ 
 }
 
 void loop() {
@@ -27,5 +32,10 @@ void sendADCValue() {
   int nextAdcValue = analogRead(ADC_PIN_NEXT);   // Read ADC value
   uint8_t nextAddress = map(nextAdcValue, 0, 1023, 0, 127);
   TinyWireS.write(nextAddress);
+}
 
+void sendVarValue(){
+  varAdcValue = analogRead(ADC_PIN_VAR);
+  TinyWireS.write(varAdcValue);
+  
 }
